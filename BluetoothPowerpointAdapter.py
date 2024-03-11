b53887c329e1b44876febdbd7ce4be142c7d5bb5
Lars Richter <larsrichter41@gmail.com>
2024-03-11 13:29:39 +0100

    Refactored BluetoothPowerpointAdapter.py -Removed All Warnings

--- BluetoothPowerpointAdapter.py
+++ BluetoothPowerpointAdapter.py
@@ -5,68 +5,72 @@ import win32com.client
 from bluetooth import *
 import os
 
+from win32com.universal import com_error
+
+
 class Actions(Enum):
     OPENPPTX = b'openPPTX:'
     NEXTSLIDE = b'NextSlide'
     CLOSECONNECTION = b'CloseConnection'
     PREVIOUSLIDE = b'PreviousSlide'
+
+
 class Ppt:
-    def _startSlideShow(self):
+    def __start_slide_show(self):
         self.objCom.SlideShowSettings.Run()
 
     def __init__(self, path):
-        self.objCom = app.Presentations.Open(FileName=presentations_path+"\\"+path,
+        self.objCom = app.Presentations.Open(FileName=presentations_path + "\\" + path,
                                              WithWindow=0)
-        self._startSlideShow()
+        self.__start_slide_show()
 
-    def nextSlide(self):
+    def next_slide(self):
         try:
             self.objCom.SlideShowWindow.View.Next()
             return "done"
-        except:
+        except com_error:
             print("Presentation not available")
             return "Error: Presentation Window not open"
 
-    def previousSlide(self):
+    def previous_slide(self):
         try:
             self.objCom.SlideShowWindow.View.Previous()
             return "done"
-        except:
+        except com_error:
             return "Error: Couldn't go to previousSlide"
 
-    def stopSlideShow(self):
+    def stop_slide_show(self):
         try:
             self.objCom.SlideShowWindow.View.Exit()
             return "done"
-        except:
+        except com_error:
             return "Couldn't close Presentation"
 
 
 def handle_message(presentation_name):
     ppt = Ppt(presentation_name)
-    client_socket.send("opened:".encode() + presentation_name.encode())
+    client = client_socket
+    client.send("opened:".encode() + presentation_name.encode())
     while True:
-        _data = client_socket.recv(1024)
+        _data = client.recv(1024)
         if len(_data) == 0:
             break
         match _data:
             case Actions.NEXTSLIDE.value:
-                response = ppt.nextSlide()
-                client_socket.send(response.encode())
+                response = ppt.next_slide()
+                client.send(response.encode())
             case Actions.PREVIOUSLIDE.value:
-                client_socket.send(ppt.previousSlide().encode())
+                client.send(ppt.previous_slide().encode())
             case Actions.CLOSECONNECTION.value:
-                client_socket.send(ppt.stopSlideShow().encode())
-                socket.close()
-                break
-
+                client.send(ppt.stop_slide_show().encode())
+                client.close()
 
 
 app = win32com.client.Dispatch("PowerPoint.Application")
 socket = BluetoothSocket(RFCOMM)
 try:
     socket.bind(("", 0))
-except:
+except BluetoothError:
     print("Device does not Support Bluetooth or Bluetooth is disabled")
     time.sleep(5)
     quit(42069)
@@ -79,7 +83,6 @@ advertise_service(socket, name="Sample Server",
                   service_id="5feedd1f-2df3-404c-a1ec-b7f32a6c9b11",
                   service_classes=["5feedd1f-2df3-404c-a1ec-b7f32a6c9b11", SERIAL_PORT_CLASS],
                   profiles=[SERIAL_PORT_PROFILE])
-
 print("Waiting for connection on port", port)
 
 client_socket, client_address = socket.accept()
@@ -99,4 +102,7 @@ while True:
         dataArr = data.split(b':')
         decoded = dataArr[1].decode()
         handle_message(decoded)
+        break
+    elif Actions.CLOSECONNECTION.value == data:
+        client_socket.close()
         break
