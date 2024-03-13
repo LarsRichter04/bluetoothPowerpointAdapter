973ef3e60b972d909dfaf639b8597c688b09a15f
Richter, Lars  SH/HZA-HAADD <richtlrs@schaeffler.com>
2024-03-13 14:02:45 +0100

    completed first Refactor of BluetoothPowerpointAdapter.py

--- BluetoothPowerpointAdapter.py
+++ BluetoothPowerpointAdapter.py
@@ -10,14 +10,17 @@ from win32com.universal import com_error
 
 class Answers(Enum):
     OPENPPTX = b'0'
-    NEXTSLIDE = b'1'
-    CLOSECONNECTION = b'3'
-    PREVIOUSLIDE = b'2'
+    NEXTSLIDE = b'\x01'
+    CLOSECONNECTION = b'\x03'
+    PREVIOUSLIDE = b'\x02'
 
 
 class Commands(Enum):
     CONNECTIONESTABLISHED = b'0'
     DONEACTION = b'1'
+    OPENED = b'2'
+    ERROR = b'3'
+
 
 
 class Ppt:
@@ -32,42 +35,44 @@ class Ppt:
     def next_slide(self):
         try:
             self.objCom.SlideShowWindow.View.Next()
-            return "done"
+            return Commands.DONEACTION.value
         except com_error:
             print("Presentation not available")
-            return "Error: Presentation Window not open"
+            return Commands.ERROR.value
 
     def previous_slide(self):
         try:
             self.objCom.SlideShowWindow.View.Previous()
-            return "done"
+            return Commands.DONEACTION.value
         except com_error:
-            return "Error: Couldn't go to previousSlide"
+            return Commands.ERROR.value
 
     def stop_slide_show(self):
         try:
             self.objCom.SlideShowWindow.View.Exit()
-            return "done"
+            return Commands.DONEACTION.value
         except com_error:
-            return "Couldn't close Presentation"
+            return Commands.ERROR.value
 
 
-def handle_message(presentation_name):
-    ppt = Ppt(presentation_name)
+def handle_message(presentation_index):
+    presentation_index = int(presentation_index)
+    ppt = Ppt(presentations[presentation_index])
     client = client_socket
-    client.send("opened:".encode() + presentation_name.encode())
+    client.send(Commands.OPENED.value + b':' + presentations[presentation_index].encode())
     while True:
         _data = client.recv(1024)
+        print(_data)
         if len(_data) == 0:
             break
         match _data:
             case Answers.NEXTSLIDE.value:
                 response = ppt.next_slide()
-                client.send(response.encode())
+                client.send(response)
             case Answers.PREVIOUSLIDE.value:
-                client.send(ppt.previous_slide().encode())
+                client.send(ppt.previous_slide())
             case Answers.CLOSECONNECTION.value:
-                client.send(ppt.stop_slide_show().encode())
+                client.send(ppt.stop_slide_show())
                 client.close()
                 break
 
@@ -81,23 +86,22 @@ except BluetoothError:
     time.sleep(5)
     quit(42069)
 socket.listen(1)
-
-presentations_path = os.getcwd()
-allPresentations = os.listdir(presentations_path)
 port = socket.getsockname()[1]
+uuid = "5feedd1f-2df3-404c-a1ec-b7f32a6c9b11"
 advertise_service(socket, name="Bluetooth Powerpoint Adapter",
-                  service_id="5feedd1f-2df3-404c-a1ec-b7f32a6c9b11",
-                  service_classes=["5feedd1f-2df3-404c-a1ec-b7f32a6c9b11", SERIAL_PORT_CLASS],
+                  service_id=uuid,
+                  service_classes=[uuid, SERIAL_PORT_CLASS],
                   profiles=[SERIAL_PORT_PROFILE])
 print("Waiting for connection on port", port)
-
 client_socket, client_address = socket.accept()
+presentations_path = os.getcwd()
+allPresentations = os.listdir(presentations_path)
+presentations = [x for x in allPresentations if ".pptx" in x]
+presentation_string = ";".join(presentations)
+
+print(presentation_string)
 print("Accepted connection from", client_address)
-presentation_string = ""
-for presentation in allPresentations:
-    if ".pptx" in presentation:
-        presentation_string += presentation + ";"
-client_socket.send("Connection established:" + presentation_string)
+client_socket.send(Commands.CONNECTIONESTABLISHED.value + b":" + presentation_string.encode())
 
 while True:
     data = client_socket.recv(1024)
